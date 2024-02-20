99f2792c76a297a916703d6dbca167db9784528d
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-02-19 21:15:29 -0300

    corrigido-erro-nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Header } from "./components/Header"
-import { Login } from "./pages/Login"
-import GlobalStyles from "./styles/GlobalStyles"
-import Routes from "./routes"
+import { BrowserRouter as Router } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+import { Header } from "./components/Header";
+import Routes from "./routes/index";
 
 function App() {
 
 
   return (
-    <>
+    <Router>
+        <Header />
         <Routes />
         <GlobalStyles />
-    </>
+    </Router>
   )
 }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Header} from "./../components/Header"
+import { Switch, Route } from "react-router-dom";
 
 import { Login } from "../pages/Login"
 import { Page404 } from "../pages/Page404"
 
 export default function Routes(){
     return(
-        <BrowserRouter>
-            <Header />
-            <Switch>
-                <Route exact path="/" component={Login} />
-                <Route exact path="/login" component={Login} />
-                <Route  path="*" component={Page404} />
-            </Switch>
-        </BrowserRouter>
+        <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={Login} />
+            <Route path="*" component={Page404} />
+        </Switch>
     )
 }
